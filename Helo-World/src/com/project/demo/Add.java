fb61576f27ab8df33f2ed86df9514ffa0e3aa30e
Preethi18777 <preethigowda1877@gmail.com>
2022-09-29 22:27:05 +0530

    additional

---
+++ Helo-World/src/com/project/demo/Add.java
@@ -0,0 +1,15 @@
+package com.project.demo;
+
+public class Add {
+
+	public static void main(String[] args) 
+	{
+		int a=2;
+		int b=3;
+		int sum = a+b;
+		{
+		System.out.println(sum);	
+		}
+	}
+
+}
