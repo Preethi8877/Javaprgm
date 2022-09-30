310e79ad3385460b80d6a0a0312c421c1720fe49
Preethi18777 <preethigowda1877@gmail.com>
2022-09-30 14:57:20 +0530

    Swapping

---
+++ First-Prgrm/src/Javaprgrm/Swapping.java
@@ -0,0 +1,17 @@
+package Javaprgrm;
+
+public class Swapping {
+
+	public static void main(String[] args)
+	{
+
+  int a=30;
+  int b=40;
+  a= a+b;
+  b=a-b;
+  a=a-b;
+  System.out.println(a);
+  System.out.println(b);
+	}
+
+}
